bd49302da2116bdb43834cd20d65257c1ef57e39
Ajand Mardalizad <aseedgeek@gmail.com>
2023-06-06 19:00:50 +0400

    basic templates added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,71 @@
-import '../styles/globals.css'
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+const darkTheme = createTheme({
+  typography: {
+    fontFamily: "Dosis",
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#65CBBF",
+      paper: "#493E7F",
+    },
+    lavendar: {
+      main: "#8167FF",
+      light: "#B3A8FF",
+    },
+    secondary: {
+      main: "#F4D06F",
+    },
+    background: {
+      default: "#070A52",
+      paper: "#28203F",
+      secondary: "#443569",
+    },
+    error: {
+      main: "#FF004F",
+    },
+    pink: {
+      main: "#FC1E9A",
+      light: "#fd8ecc",
+    },
+    simple: {
+      main: "#F5FBEF",
+      contrastText: "#000",
+    },
+    error: {
+      main: "#e76d89",
+    },
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          borderRadius: 10,
+          boxShadow: "rgb(20 21 33 / 18%) 0px 2px 10px 0px",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: { borderRadius: 8 },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: { borderRadius: "8px !important" },
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
